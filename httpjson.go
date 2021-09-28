42210691ee49249fea77a0f2bf9e915de29fba1a
aliforever <alipadida@live.com>
2021-09-28 19:08:15 +0330

    removed the need for raw response

--- httpjson.go
+++ httpjson.go
@@ -5,36 +5,36 @@ import (
 )
 
 func Ok(writer http.ResponseWriter, data interface{}) (err error) {
-	err = writeHeadersAndData(writer, http.StatusOK, false, data)
+	err = writeHeadersAndData(writer, http.StatusOK, data)
 	return
 }
 
 func Accepted(writer http.ResponseWriter, data interface{}) (err error) {
-	err = writeHeadersAndData(writer, http.StatusAccepted, false, &data)
+	err = writeHeadersAndData(writer, http.StatusAccepted, data)
 	return
 }
 
 func BadRequest(writer http.ResponseWriter, data interface{}) (err error) {
-	err = writeHeadersAndData(writer, http.StatusBadRequest, false, &data)
+	err = writeHeadersAndData(writer, http.StatusBadRequest, data)
 	return
 }
 
 func Unauthorized(writer http.ResponseWriter, data interface{}) (err error) {
-	err = writeHeadersAndData(writer, http.StatusUnauthorized, false, &data)
+	err = writeHeadersAndData(writer, http.StatusUnauthorized, data)
 	return
 }
 
 func Forbidden(writer http.ResponseWriter, data interface{}) (err error) {
-	err = writeHeadersAndData(writer, http.StatusForbidden, false, &data)
+	err = writeHeadersAndData(writer, http.StatusForbidden, data)
 	return
 }
 
 func NotFound(writer http.ResponseWriter, data interface{}) (err error) {
-	err = writeHeadersAndData(writer, http.StatusNotFound, false, &data)
+	err = writeHeadersAndData(writer, http.StatusNotFound, data)
 	return
 }
 
 func MethodNotAllowed(writer http.ResponseWriter, data interface{}) (err error) {
-	err = writeHeadersAndData(writer, http.StatusMethodNotAllowed, false, &data)
+	err = writeHeadersAndData(writer, http.StatusMethodNotAllowed, data)
 	return
 }
--- response.go
+++ response.go
@@ -2,7 +2,6 @@ package httpjson
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -12,56 +11,21 @@ type response struct {
 	Data       interface{} `json:"data,omitempty"`
 }
 
-func writeHeadersAndData(writer http.ResponseWriter, statusCode int, isRaw bool, data interface{}) (err error) {
-	if !isRaw {
-		var jsonBytes []byte
-		jsonBytes, err = json.Marshal(response{
-			StatusCode: statusCode,
-			StatusName: statusMessage(statusCode),
-			Data:       data,
-		})
-		if returnOnJsonMarshalError(writer, err) {
-			return
-		}
-		writer.Header().Set("Content-Type", "application/json")
-		writer.WriteHeader(statusCode)
-		writer.Write(jsonBytes)
-	} else {
-		writer.Header().Set("Content-Type", "application/text")
-		writer.WriteHeader(statusCode)
-
-		var r []byte
-
-		switch data.(type) {
-		case string:
-			r = []byte(data.(string))
-			break
-		case uint, uint8, uint16, uint32, uint64, int, int8, int32, int64:
-			r = []byte(fmt.Sprintf("%d", data.(int)))
-			break
-		case float32, float64:
-			r = []byte(fmt.Sprintf("%f", data))
-			break
-		case bool:
-			r = []byte(fmt.Sprintf("%t", data.(bool)))
-			break
-		default:
-			r, _ = json.Marshal(data)
-			if r == nil {
-				// TODO: It might be better to return Internal Server Error and report unknown types
-				r = []byte("EMPTY_RESPONSE")
-			}
-		}
-		writer.Write(r)
-	}
-	return
-}
-
-func returnOnJsonMarshalError(writer http.ResponseWriter, err error) (shouldReturn bool) {
+func writeHeadersAndData(writer http.ResponseWriter, statusCode int, data interface{}) (err error) {
+	var jsonBytes []byte
+	jsonBytes, err = json.Marshal(response{
+		StatusCode: statusCode,
+		StatusName: statusMessage(statusCode),
+		Data:       data,
+	})
 	if err != nil {
-		shouldReturn = true
-		message := "Internal Server Error"
-		writeHeadersAndData(writer, http.StatusInternalServerError, true, &message)
+		writer.Header().Set("Content-Type", "application/text")
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte("Internal Server Error"))
+		return
 	}
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+	writer.Write(jsonBytes)
 	return
 }
