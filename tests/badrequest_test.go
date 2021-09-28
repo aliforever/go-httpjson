9957d99f334c2396924339082f1dd37003996bc2
aliforever <alipadida@live.com>
2021-09-28 19:23:36 +0330

    completed unit tests

---
+++ tests/badrequest_test.go
@@ -0,0 +1,46 @@
+package tests
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/aliforever/go-httpjson"
+)
+
+func TestBadRequest(t *testing.T) {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		httpjson.BadRequest(writer, "Welcome!")
+	})
+	go http.ListenAndServe(":80", nil)
+
+	resp, err := http.Get("http://localhost")
+	if err != nil {
+		t.Fatal("Error sending http request", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("Error reading response", err)
+		return
+	}
+
+	expected := fmt.Sprintf(`{"status_code":%d,"status_name":"BAD_REQUEST","data":"Welcome!"}`, http.StatusBadRequest)
+	value := string(body)
+
+	if expected != value {
+		t.Fatal(fmt.Sprintf("Test Failed!\nNot the expected outcome!\nExpected: %s\nResult: %s", expected, value))
+		return
+	}
+
+	expectedResultCode := http.StatusBadRequest
+	valueResultCode := resp.StatusCode
+
+	if expectedResultCode != valueResultCode {
+		t.Fatal(fmt.Sprintf("Test Failed\nNot the expected Result Code.\nExpected :%d\nValue: %d", expectedResultCode, valueResultCode))
+	}
+}
---
+++ tests/ok_test.go
@@ -0,0 +1,46 @@
+package tests
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/aliforever/go-httpjson"
+)
+
+func TestOk(t *testing.T) {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		httpjson.Ok(writer, "Welcome!")
+	})
+	go http.ListenAndServe(":80", nil)
+
+	resp, err := http.Get("http://localhost")
+	if err != nil {
+		t.Fatal("Error sending http request", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("Error reading response", err)
+		return
+	}
+
+	expected := fmt.Sprintf(`{"status_code":%d,"status_name":"OK","data":"Welcome!"}`, http.StatusOK)
+	value := string(body)
+
+	if expected != value {
+		t.Fatal(fmt.Sprintf("Test Failed!\nNot the expected outcome!\nExpected: %s\nResult: %s", expected, value))
+		return
+	}
+
+	expectedResultCode := http.StatusOK
+	valueResultCode := resp.StatusCode
+
+	if expectedResultCode != valueResultCode {
+		t.Fatal(fmt.Sprintf("Test Failed\nNot the expected Result Code.\nExpected :%d\nValue: %d", expectedResultCode, valueResultCode))
+	}
+}
