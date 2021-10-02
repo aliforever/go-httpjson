6dbbf396939a54d74d072b2f597f1def2840aeb7
aliforever <alipadida@live.com>
2021-10-02 13:44:42 +0330

    made Request fields private

--- request.go
+++ request.go
@@ -6,21 +6,36 @@ import (
 )
 
 type Request struct {
-	Action string          `json:"action"`
-	Data   json.RawMessage `json:"data"`
+	action string
+	data   json.RawMessage
 }
 
 func ParseRequest(wr http.ResponseWriter, r *http.Request) (req *Request, err error) {
+	type Req struct {
+		Action string          `json:"action"`
+		Data   json.RawMessage `json:"data"`
+	}
+
+	var publicReq *Req
 	defer r.Body.Close()
 	decoder := json.NewDecoder(r.Body)
-	err = decoder.Decode(&req)
+	err = decoder.Decode(&publicReq)
 	if err != nil {
 		BadRequest(wr, "invalid_json")
+		return
+	}
+	req = &Request{
+		action: publicReq.Action,
+		data:   publicReq.Data,
 	}
 	return
 }
 
+func (r *Request) Action() string {
+	return r.action
+}
+
 func (r *Request) UnmarshalData(dst interface{}) (err error) {
-	err = json.Unmarshal(r.Data, &dst)
+	err = json.Unmarshal(r.data, &dst)
 	return
 }
