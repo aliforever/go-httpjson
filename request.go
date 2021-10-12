49367fd55c49ac7ab12d68bca503e56c4d3c7e5e
aliforever <alipadida@live.com>
2021-10-12 19:50:55 +0330

    letting user pass custom struct to be more generic

--- request.go
+++ request.go
@@ -5,37 +5,13 @@ import (
 	"net/http"
 )
 
-type Request struct {
-	action string
-	data   json.RawMessage
-}
-
-func ParseRequest(wr http.ResponseWriter, r *http.Request) (req *Request, err error) {
-	type Req struct {
-		Action string          `json:"action"`
-		Data   json.RawMessage `json:"data"`
-	}
-
-	var publicReq *Req
+func ParseRequest(wr http.ResponseWriter, r *http.Request, destination interface{}) (err error) {
 	defer r.Body.Close()
 	decoder := json.NewDecoder(r.Body)
-	err = decoder.Decode(&publicReq)
+	err = decoder.Decode(&destination)
 	if err != nil {
 		BadRequest(wr, "invalid_json")
 		return
 	}
-	req = &Request{
-		action: publicReq.Action,
-		data:   publicReq.Data,
-	}
-	return
-}
-
-func (r *Request) Action() string {
-	return r.action
-}
-
-func (r *Request) UnmarshalData(dst interface{}) (err error) {
-	err = json.Unmarshal(r.data, &dst)
 	return
 }
