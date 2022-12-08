7cfa0ba468eddbf4234fb55be3cdf87c294f9f71
aliforever <alipadida@live.com>
2022-12-08 20:50:51 +0300

    added post method

--- request.go
+++ request.go
@@ -1,6 +1,7 @@
 package httpjson
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 )
@@ -11,4 +12,23 @@ func UnmarshalRequest[T any](r *http.Request) (t *T, err error) {
 	err = json.NewDecoder(r.Body).Decode(&t)
 
 	return
+}
+
+func Post[Response any](address string, data interface{}) (*Response, error) {
+	body, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.Post(address, "application/json", bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var r *Response
+
+	err = json.NewDecoder(resp.Body).Decode(&r)
+
+	return r, err
 }
