a2d83a4bbc57ddf820f43e60f7c1220daf6539cf
aliforever <alipadida@live.com>
2023-01-20 12:26:27 +0300

    added methods to unmarshal from bytes

--- request.go
+++ request.go
@@ -7,6 +7,12 @@ import (
 	"net/http"
 )
 
+type Request struct {
+	payload
+
+	Data json.RawMessage `json:"data,omitempty"`
+}
+
 func UnmarshalRequest[T any](r *http.Request) (t *T, err error) {
 	defer r.Body.Close()
 
@@ -15,18 +21,34 @@ func UnmarshalRequest[T any](r *http.Request) (t *T, err error) {
 	return
 }
 
-type Request struct {
-	payload
-
-	Data json.RawMessage `json:"data,omitempty"`
-}
-
 // UnmarshalRequestPayload this is to unmarshal the request using the library's payload structure
 func UnmarshalRequestPayload[T any](r *http.Request) (t *T, err error) {
 	defer r.Body.Close()
 
 	var request *Request
 	err = json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(request.Data, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func UnmarshalFromBytes[T any](data []byte) (t *T, err error) {
+	err = json.Unmarshal(data, &t)
+
+	return
+}
+
+// UnmarshalPayloadFromBytes this is to unmarshal the request using the library's payload structure
+func UnmarshalPayloadFromBytes[T any](data []byte) (t *T, err error) {
+	var request *Request
+	err = json.Unmarshal(data, &request)
 	if err != nil {
 		return nil, err
 	}
