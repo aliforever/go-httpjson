ae9f5f13ab06ca608a911d9a0282c6905f684abb
aliforever <alipadida@live.com>
2023-01-20 10:28:53 +0300

    removed writer from new method

--- builder.go
+++ builder.go
@@ -6,13 +6,12 @@ import (
 )
 
 type Builder struct {
-	writer  http.ResponseWriter
 	message string
 	data    interface{}
 }
 
-func New(writer http.ResponseWriter) Builder {
-	return Builder{writer: writer}
+func New() Builder {
+	return Builder{}
 }
 
 func (b *Builder) SetMessage(message string) *Builder {
@@ -30,34 +29,34 @@ func (b *Builder) JSON(statusCode int) ([]byte, error) {
 	return json.Marshal(newResponse(statusCode, b.message, b.data))
 }
 
-func (b *Builder) Ok() error {
-	return Ok(b.writer, b.message, b.data)
+func (b *Builder) Ok(writer http.ResponseWriter) error {
+	return Ok(writer, b.message, b.data)
 }
 
-func (b *Builder) Accepted() error {
-	return Accepted(b.writer, b.message, b.data)
+func (b *Builder) Accepted(writer http.ResponseWriter) error {
+	return Accepted(writer, b.message, b.data)
 }
 
-func (b *Builder) BadRequest() error {
-	return BadRequest(b.writer, b.message, b.data)
+func (b *Builder) BadRequest(writer http.ResponseWriter) error {
+	return BadRequest(writer, b.message, b.data)
 }
 
-func (b *Builder) Unauthorized() error {
-	return Unauthorized(b.writer, b.message, b.data)
+func (b *Builder) Unauthorized(writer http.ResponseWriter) error {
+	return Unauthorized(writer, b.message, b.data)
 }
 
-func (b *Builder) Forbidden() error {
-	return Forbidden(b.writer, b.message, b.data)
+func (b *Builder) Forbidden(writer http.ResponseWriter) error {
+	return Forbidden(writer, b.message, b.data)
 }
 
-func (b *Builder) NotFound() error {
-	return NotFound(b.writer, b.message, b.data)
+func (b *Builder) NotFound(writer http.ResponseWriter) error {
+	return NotFound(writer, b.message, b.data)
 }
 
-func (b *Builder) MethodNotAllowed() error {
-	return MethodNotAllowed(b.writer, b.message, b.data)
+func (b *Builder) MethodNotAllowed(writer http.ResponseWriter) error {
+	return MethodNotAllowed(writer, b.message, b.data)
 }
 
-func (b *Builder) InternalServerError() error {
-	return InternalServerError(b.writer, b.message, b.data)
+func (b *Builder) InternalServerError(writer http.ResponseWriter) error {
+	return InternalServerError(writer, b.message, b.data)
 }
