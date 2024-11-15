4d822f4eedc36117479b040dc7412a23a63dcd38
aliforever <alipadida@live.com>
2024-11-15 13:35:22 +0300

    added custom methods

---
+++ httpjsoncustom.go
@@ -0,0 +1,45 @@
+package httpjson
+
+import (
+	"net/http"
+)
+
+func OkCustom(writer http.ResponseWriter, data interface{}) (err error) {
+	err = writeHeadersAndDataCustom(writer, http.StatusOK, data)
+	return
+}
+
+func AcceptedCustom(writer http.ResponseWriter, data interface{}) (err error) {
+	err = writeHeadersAndDataCustom(writer, http.StatusAccepted, data)
+	return
+}
+
+func BadRequestCustom(writer http.ResponseWriter, data interface{}) (err error) {
+	err = writeHeadersAndDataCustom(writer, http.StatusBadRequest, data)
+	return
+}
+
+func UnauthorizedCustom(writer http.ResponseWriter, data interface{}) (err error) {
+	err = writeHeadersAndDataCustom(writer, http.StatusUnauthorized, data)
+	return
+}
+
+func ForbiddenCustom(writer http.ResponseWriter, data interface{}) (err error) {
+	err = writeHeadersAndDataCustom(writer, http.StatusForbidden, data)
+	return
+}
+
+func NotFoundCustom(writer http.ResponseWriter, data interface{}) (err error) {
+	err = writeHeadersAndDataCustom(writer, http.StatusNotFound, data)
+	return
+}
+
+func MethodNotAllowedCustom(writer http.ResponseWriter, data interface{}) (err error) {
+	err = writeHeadersAndDataCustom(writer, http.StatusMethodNotAllowed, data)
+	return
+}
+
+func InternalServerErrorCustom(writer http.ResponseWriter, data interface{}) (err error) {
+	err = writeHeadersAndDataCustom(writer, http.StatusInternalServerError, data)
+	return
+}
--- response.go
+++ response.go
@@ -24,6 +24,7 @@ func newResponse(statusCode int, message string, data interface{}) response {
 
 func writeHeadersAndData(writer http.ResponseWriter, statusCode int, message string, data interface{}) (err error) {
 	var jsonBytes []byte
+
 	jsonBytes, err = json.Marshal(newResponse(statusCode, message, data))
 	if err != nil {
 		return err
@@ -31,6 +32,23 @@ func writeHeadersAndData(writer http.ResponseWriter, statusCode int, message str
 
 	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(statusCode)
+
+	_, err = writer.Write(jsonBytes)
+
+	return err
+}
+
+func writeHeadersAndDataCustom(writer http.ResponseWriter, statusCode int, data interface{}) (err error) {
+	var jsonBytes []byte
+
+	jsonBytes, err = json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+
 	_, err = writer.Write(jsonBytes)
 
 	return err
